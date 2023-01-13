101c571cd7194dd1b07f198e92b4018ca648a651
VPesprit <vpesprit@gmail.com>
2023-01-13 20:42:13 +0700

    Palindrom2 created
    Почему-то выводит только "В"

---
+++ pal2.py
@@ -0,0 +1,27 @@
+def palindrom(fname):
+    cnt=0
+    output=list()
+    j=0
+    with open(fname, 'r', encoding='utf8') as file:
+        for line in file:         
+            for i in range (len(line)):
+                _index=line.find(" ")
+                if _index!=-1:
+                    output.append(line[:_index+1])
+                    line=line[_index+1:]
+                else:
+                    
+                    break
+                
+            
+    
+    file.close() 
+    for i in range (0,len(output[i])):
+        rev=output[i]
+        rev=rev[::-1] 
+        if output[i].lower().strip()==rev.lower().strip():
+            print(output[i])
+               
+
+
+palindrom('C:/Users/vpesp/VC projects/PythonCourse/text2.txt') 
